9795d86d00be8838f0ef73022ab6568eca2232d7
Kostia Nahula <kostannagula@gmail.com>
2019-02-02 11:58:33 +0200

    Setup slider

--- resources/assets/src/components/Home/Home.js
+++ resources/assets/src/components/Home/Home.js
@@ -1,5 +1,4 @@
-import React, {Component, Fragment} from 'react';
-import { MDBBtn } from "mdbreact";
+import React, {Component} from 'react';
 
 import Carousel from './scenes/components/Carousel';
 import ServicesCard from './scenes/components/ServicesCard';
