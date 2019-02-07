b2fee74c52309762eacf04444f4642a4b651d39c
Kostia Nahula <kostannagula@gmail.com>
2019-02-07 21:01:27 +0200

    Created service header

--- resources/assets/src/components/Services/scenes/components/Header/Header.js
+++ resources/assets/src/components/Services/scenes/components/Header/Header.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
-
+import Grid from '@material-ui/core/Grid';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
-
+    root: {
+        flexGrow: 1
+    },
+    header: {
+        height: 400,
+        background: 'linear-gradient(rgba(127, 114, 114, 0.45), rgba(127, 114, 114, 0.45)), url(http://t.commonsupport.com/catania/images/background/4.jpg) no-repeat center',
+        fontSize: 60,
+        color: '#eee'
+    }
 });
 
+/**
+ * Header component
+ */
 class Header extends Component {
+
+    /**
+     * Constructor
+     * @param props
+     */
     constructor(props) {
         super(props);
     }
 
-    render () {
+    render() {
+
+        const {classes} = this.props;
+
         return (
-            <div>Service header component</div>
+            <div className={classes.root}>
+                <Grid className={classes.header} container justify={'center'} alignItems={'center'}>
+                    <p>Services</p>
+                </Grid>
+            </div>
         );
     }
 }
