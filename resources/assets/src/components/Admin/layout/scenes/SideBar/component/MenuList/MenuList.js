b686e891630f47801a4ba40af33acf850f1e0dbb
Kostia Nahula <kostannagula@gmail.com>
2019-03-19 20:42:42 +0200

    Updated sidebar icon

--- resources/assets/src/components/Admin/layout/scenes/SideBar/component/MenuList/MenuList.js
+++ resources/assets/src/components/Admin/layout/scenes/SideBar/component/MenuList/MenuList.js
@@ -3,29 +3,32 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import PersonIcon from '@material-ui/icons/Person';
+import MessageIcon from '@material-ui/icons/Message';
 import MailIcon from '@material-ui/icons/Mail';
+import BuildIcon from '@material-ui/icons/Build';
 import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 
 export const MenuList = () => (
     <div>
         <Divider />
         <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                    <ListItemText primary={text} />
-                </ListItem>
-            ))}
-        </List>
-        <Divider />
-        <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                    <ListItemText primary={text} />
-                </ListItem>
-            ))}
+            <ListItem button key={'users'}>
+                <ListItemIcon><PersonIcon/></ListItemIcon>
+                <ListItemText primary={'Users'} />
+            </ListItem>
+            <ListItem button key={'services'}>
+                <ListItemIcon><BuildIcon/></ListItemIcon>
+                <ListItemText primary={'Services'} />
+            </ListItem>
+            <ListItem button key={'comments'}>
+                <ListItemIcon><MessageIcon/></ListItemIcon>
+                <ListItemText primary={'Comments'} />
+            </ListItem>
+            <ListItem button key={'Mails'}>
+                <ListItemIcon><MailIcon/></ListItemIcon>
+                <ListItemText primary={'Mails'} />
+            </ListItem>
         </List>
     </div>
 );
