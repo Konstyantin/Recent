c49307a27acbf91df74b4fe2528f9a96bbdddfd2
Kostia Nahula <kostannagula@gmail.com>
2019-03-20 06:40:02 +0200

    Adde Service component

--- resources/assets/src/_routes/admin.js
+++ resources/assets/src/_routes/admin.js
@@ -1,6 +1,7 @@
 import {default as AdminServices} from '../components/Admin/scenes/components/Services'
 import {default as AdminDashboard} from '../components/Admin/scenes/components/Dashboard'
 import {Users} from "../components/Admin/components/Users";
+import {Services} from "../components/Admin/components/Services";
 
 export const adminRoutes = [
     {
@@ -19,5 +20,9 @@ export const adminRoutes = [
     {
         path: '/admin/users',
         component: Users
+    },
+    {
+        path: '/admin/services',
+        component: Services
     }
 ];
---
+++ resources/assets/src/components/Admin/components/Services/Services.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Services extends Component {
+    constructor(porps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Admin service component</div>
+        );
+    }
+}
+
+export {Services};
