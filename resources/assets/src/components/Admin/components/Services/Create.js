440ce7420f894044a22026149b8e61c6a7f77345
Kostia Nahula <kostannagula@gmail.com>
2019-03-20 20:35:15 +0200

    Create base form for create new services

--- resources/assets/src/components/Admin/components/Services/Create.js
+++ resources/assets/src/components/Admin/components/Services/Create.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
 import {withStyles} from "@material-ui/core/styles/index";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
 
 const styles = theme => ({
     root: {
         display: 'flex',
-        padding: 20
-    }
+        padding: 20,
+        flexWrap: 'wrap',
+    },
+    textField: {
+        width: '100%',
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
 });
 
 class Create extends Component {
@@ -17,8 +30,31 @@ class Create extends Component {
         return (
             <div className={classes.root}>
                 <Grid container>
-                    <Grid item xs={12}>
-                        <div>Create service component</div>
+                    <Grid item xs={8}>
+                        <TextField
+                            id="service-title"
+                            label="Title"
+                            className={classes.textField}
+                            type="text"
+                            autoComplete="service title"
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TextField
+                            id="service-description"
+                            label="Description"
+                            className={classes.textField}
+                            type="text"
+                            multiline
+                            autoComplete="service description"
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Button variant="contained" color="primary" className={classes.button}>
+                            Submit
+                        </Button>
                     </Grid>
                 </Grid>
             </div>
