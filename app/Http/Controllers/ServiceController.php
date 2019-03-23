f1005f253b50c456765c6c83b15511de2c78fb57
Kostia Nahula <kostannagula@gmail.com>
2019-03-23 15:34:59 +0200

    Implemented a create new service action

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 /**
  * Class ServiceController
@@ -20,9 +22,33 @@ class ServiceController extends Controller
         dd('service show action');
     }
 
+    /**
+     * Store action method
+     *
+     * The method responsible for create new service item
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
-        dd('service store action');
+        $input = $request->all();
+
+        $baseImageFolder = public_path() . '/images/vendor/services/';
+
+        $iconImage = $request->file('icon');
+        $iconName = $iconImage->getClientOriginalName();
+        $iconImage->move($baseImageFolder . 'icon/', $iconName);
+        $input['icon'] = $iconName;
+
+        $serviceImage = $request->file('image');
+        $imageName = $serviceImage->getClientOriginalName();
+        $serviceImage->move($baseImageFolder . 'image/', $imageName);
+        $input['image'] = $imageName;
+
+        $service = Service::create($input)->toArray();
+
+        return Response::json($service, 201);
     }
 
     public function update(Request $request, $id)
