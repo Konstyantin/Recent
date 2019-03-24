eabe3d8246382ac39d298503885314d574ccca87
Kostia Nahula <kostannagula@gmail.com>
2019-03-24 13:19:53 +0200

    Added validation rules for text field service form

--- resources/assets/src/components/Admin/scenes/components/Services/scenes/components/Create/Create.js
+++ resources/assets/src/components/Admin/scenes/components/Services/scenes/components/Create/Create.js
@@ -6,7 +6,8 @@ import Button from '@material-ui/core/Button';
 import { Editor } from '@tinymce/tinymce-react';
 import InputLabel from '@material-ui/core/InputLabel';
 import {DropzoneArea} from 'material-ui-dropzone'
-
+import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import {userActions} from "../../../../../../../../_actions";
 
 const styles = theme => ({
     root: {
@@ -31,13 +32,41 @@ class Create extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            title: '',
+            short_description: '',
+            description: '',
             icon: null,
             image: null,
+            submitted: false
         };
 
+        this.handleChange = this.handleChange.bind(this);
         this.handleChangeEditor = this.handleChangeEditor.bind(this);
         this.handleChangeIcon = this.handleChangeIcon.bind(this);
         this.handleChangeImage = this.handleChangeImage.bind(this);
+    }
+
+    /**
+     * Handle change
+     *
+     * @param e
+     */
+    handleChange(e) {
+        const {name, value} = e.target;
+        this.setState({[name]: value});
+    }
+
+    /**
+     * Handle submit form
+     *
+     * @param e
+     */
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({submitted: true});
+
+        console.log('submit service form');
     }
 
     handleChangeEditor(e) {
@@ -60,56 +89,80 @@ class Create extends Component {
 
         const {classes} = this.props;
 
+        const {title, short_description, description, icon, image} = this.state;
+
         return (
             <div className={classes.root}>
                 <Grid container>
                     <Grid item xs={8}>
-                        <Grid container spacing={16}>
-                            <Grid item xs={6}>
-                                <TextField
-                                    id="service-title"
-                                    label="Title"
-                                    className={classes.textField}
-                                    type="text"
-                                    autoComplete="service title"
-                                    margin="normal"
-                                />
+                        <ValidatorForm
+                            ref="form"
+                            onSubmit={this.handleSubmit}
+                            onError={errors => console.log(errors)}
+                        >
+                            <Grid container spacing={16}>
+                                <Grid item xs={6}>
+                                    <TextValidator
+                                        id="service-title"
+                                        label="Title"
+                                        onChange={this.handleChange}
+                                        className={classes.textField}
+                                        name="title"
+                                        margin="normal"
+                                        placeholder="Title"
+                                        value={title}
+                                        validators={['required', 'minStringLength:5', 'maxStringLength:45']}
+                                        errorMessages={[
+                                            'this field is required',
+                                            'input must be more 5 letters',
+                                            'input must be less 5 letters'
+                                        ]}
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextValidator
+                                        id="service-short-description"
+                                        label="Short description"
+                                        onChange={this.handleChange}
+                                        className={classes.textField}
+                                        name="short_description"
+                                        margin="normal"
+                                        placeholder="Short description"
+                                        value={short_description}
+                                        validators={['required', 'minStringLength:5', 'maxStringLength:45']}
+                                        errorMessages={[
+                                            'this field is required',
+                                            'input must be more 5 letters',
+                                            'input must be less 5 letters'
+                                        ]}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <InputLabel>Description</InputLabel>
+                                    <Editor
+                                        initialValue="<p>This is the initial content of the editor</p>"
+                                        init={{
+                                            plugins: 'link image code',
+                                            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                                        }}
+                                        onChange={this.handleChangeEditor}
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <InputLabel>Service Icon</InputLabel>
+                                    <DropzoneArea onChange={this.handleChangeIcon}/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <InputLabel>Service Image</InputLabel>
+                                    <DropzoneArea onChange={this.handleChangeIcon}/>
+                                </Grid>
+                                <Grid item xs={8}>
+                                    <Button variant="contained" color="primary" className={classes.button} type={'submit'}>
+                                        Submit
+                                    </Button>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    id="service-short-description"
-                                    label="Short description"
-                                    className={classes.textField}
-                                    type="text"
-                                    autoComplete="service short description"
-                                    margin="normal"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <InputLabel>Description</InputLabel>
-                                <Editor
-                                    initialValue="<p>This is the initial content of the editor</p>"
-                                    init={{
-                                        plugins: 'link image code',
-                                        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-                                    }}
-                                    onChange={this.handleChangeEditor}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <InputLabel>Service Icon</InputLabel>
-                                <DropzoneArea onChange={this.handleChangeIcon}/>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <InputLabel>Service Image</InputLabel>
-                                <DropzoneArea onChange={this.handleChangeIcon}/>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <Button variant="contained" color="primary" className={classes.button}>
-                                    Submit
-                                </Button>
-                            </Grid>
-                        </Grid>
+                        </ValidatorForm>
                     </Grid>
                 </Grid>
             </div>
